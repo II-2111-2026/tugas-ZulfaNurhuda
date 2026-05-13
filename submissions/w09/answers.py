f8b07e32b084352ce75885fb7f327763e8ad5af4
ZulfaNurhuda <me@zulfanurhuda.my.id>
2026-05-13 13:41:55 +0700

    feat(w09): submit week 09 assignment

--- submissions/w09/answers.py
+++ submissions/w09/answers.py
@@ -17,19 +17,19 @@ def q01() -> bool:
     """
     [T/F] Jika korelasi antara dua variabel adalah nol, maka kedua variabel tersebut pasti independen.
     """
-    raise NotImplementedError
+    return False
 
 def q02() -> bool:
     """
     [T/F] PDF marginal fX(x) didapatkan dengan mengintegralkan fX,Y(x,y) terhadap y dari -tak hingga ke tak hingga.
     """
-    raise NotImplementedError
+    return True
 
 def q03() -> bool:
     """
     [T/F] Nilai koefisien korelasi selalu berada di antara -1 dan 1.
     """
-    raise NotImplementedError
+    return True
 
 def q04() -> str:
     """
@@ -39,7 +39,7 @@ def q04() -> str:
      C) E[X]/E[Y]
      D) 0
     """
-    raise NotImplementedError
+    return "B"
 
 def q05() -> str:
     """
@@ -49,7 +49,7 @@ def q05() -> str:
      C) Korelasi.
      D) CDF.
     """
-    raise NotImplementedError
+    return "C"
 
 def q06() -> str:
     """
@@ -59,7 +59,7 @@ def q06() -> str:
      C) 36
      D) 6,5
     """
-    raise NotImplementedError
+    return "A"
 
 def q07() -> str:
     """
@@ -69,34 +69,34 @@ def q07() -> str:
      C) fX(x) fY(y)
      D) fX,Y(x,y) - fX(x)
     """
-    raise NotImplementedError
+    return "A"
 
 def q08() -> float:
     """
     [Numeric] Jika Cov(X,Y) = 2, sigmaX = 2, sigmaY = 2, berapakah koefisien korelasinya?
     """
-    raise NotImplementedError
+    return 0.5
 
 def q09() -> float:
     """
     [Numeric] Berapakah nilai E[X+Y] jika E[X] = 10 dan E[Y] = 20?
     """
-    raise NotImplementedError
+    return 30.0
 
 def q10() -> float:
     """
     [Numeric] Jika f(x,y) = 1/4 untuk 0 <= x <= 2 dan 0 <= y <= 2, berapakah P(X <= 1, Y <= 1)?
     """
-    raise NotImplementedError
+    return 0.25
 
 def q11() -> float:
     """
     [Numeric] Dalam tabel diskrit, jika P(1,1) = 0,1, P(1,2) = 0,2, P(2,1) = 0,3, P(2,2) = 0,4, berapakah probabilitas marginal P(X = 1)?
     """
-    raise NotImplementedError
+    return 0.3
 
 def q12() -> float:
     """
     [Numeric] Jika X dan X memiliki korelasi 1 dan Var(X) = 4, berapakah Var(X+X)?
     """
-    raise NotImplementedError
+    return 16.0
